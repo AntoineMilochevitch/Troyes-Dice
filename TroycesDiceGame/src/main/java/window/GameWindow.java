70288ee14bfe458fdcb6196af1bfdf013b5f71bc
Malgawin <101182923+Malgawin@users.noreply.github.com>
2024-12-26 20:17:25 +0100

    Update GameWindow.java
    Menu + ajout player

--- TroycesDiceGame/src/main/java/window/GameWindow.java
+++ TroycesDiceGame/src/main/java/window/GameWindow.java
@@ -1,7 +1,5 @@
 package main.java.window;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class GameWindow extends JFrame{
@@ -13,26 +11,34 @@ public class GameWindow extends JFrame{
     private GameWindow(){
         game = GameInstance.getInstance();
 
-        setTitle("Game");
-        setSize(300,200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2,1));
+        this.setTitle("Troyes Dice");
+        this.setSize(1500,1000);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+
+
+        JPanel menu = new JPanel();
+        menu.setLayout(null);
+        menu.setBackground(new Color(255, 243, 230));
 
         JLabel statusLabel = new JLabel("Game Status: Not Started", SwingConstants.CENTER);
+        statusLabel.setBounds(675, 410, 150, 60);
 
         JButton startButton = new JButton("Start Game");
+        startButton.setBounds(675, 350, 150, 60);   
 
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                statusLabel.setText("Game Status : Started");
-                game.startGame();
-            }
-        });
+        startButton.addActionListener( (e) -> {
+                clearPanel(menu);
+                this.numberPlayers(menu);
+            });
 
-        add(startButton);
-        add(statusLabel);
-        setVisible(true);
+        menu.add(startButton);
+        menu.add(statusLabel);
+
+        this.add(menu, BorderLayout.CENTER);
+
+        this.setVisible(true);
     }
 
     public static GameWindow getInstance() {
@@ -49,5 +55,70 @@ public class GameWindow extends JFrame{
                 GameWindow.getInstance();
             }
         });
+    }
+
+    private void clearPanel(JPanel panel){
+        panel.removeAll();
+        panel.revalidate();
+        panel.repaint();
+    }
+
+
+    private void numberPlayers(JPanel panel){
+        JLabel playersLabel = new JLabel("Number of Players:");
+        playersLabel.setBounds(555, 350, 110, 60);
+
+        JTextField playersText = new JTextField("2");
+        playersText.setBounds(675, 365, 150, 30);
+
+        JButton confirmed = new JButton("Confirmed");
+        confirmed.setBounds(675, 410, 150, 60);   
+
+        confirmed.addActionListener( (e) -> {
+                clearPanel(panel);
+                Integer nbPlayers = Integer.parseInt(playersText.getText());
+                if (nbPlayers >= 2){
+                    this.clearPanel(panel);
+                    this.addPlayers(panel, nbPlayers);
+                }
+            });
+
+        panel.add(playersLabel);
+        panel.add(playersText);
+        panel.add(confirmed);
+    }
+
+    private void addPlayers(JPanel panel, Integer id){
+        if (id == 0){
+            this.clearPanel(panel);
+            //TO DO next state
+        }
+        else{
+            JLabel player = new JLabel("Player " + id);
+            player.setBounds(675, 290, 80, 60);
+
+            JLabel nameLabel = new JLabel("Name:");
+            nameLabel.setBounds(625, 350, 50, 60);
+
+            JTextField nameText = new JTextField("Player " + id);
+            nameText.setBounds(675, 365, 150, 30);
+
+            JButton confirmed = new JButton("Confirmed");
+            confirmed.setBounds(675, 410, 150, 60); 
+
+            confirmed.addActionListener( (e) -> {
+                clearPanel(panel);
+                String namePlayer = nameText.getText();
+                //TO DO add namePlayer to the list of players
+                this.clearPanel(panel);
+                this.addPlayers(panel, id-1);
+
+            });
+
+            panel.add(player);
+            panel.add(nameLabel);
+            panel.add(nameText);
+            panel.add(confirmed);
+        }  
     }
 }
